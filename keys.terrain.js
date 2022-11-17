a2e0cce7dcff58a31788bf5fe21250a130b084e4
Leonardo Digiorgio <simon.digiorgio@gmail.com>
2022-11-17 08:59:28 +0100

    My first terra dapp

---
+++ keys.terrain.js
@@ -0,0 +1,9 @@
+// can use `process.env.SECRET_MNEMONIC` or `process.env.SECRET_PRIV_KEY`
+// to populate secret in CI environment instead of hardcoding
+
+module.exports = {
+  pisco: {
+    mnemonic:
+      "sphere road bike toe inflict heavy still immense spoil injury people erosion pelican panic error top found ten remove proof please noble bone adjust",
+  },
+};
